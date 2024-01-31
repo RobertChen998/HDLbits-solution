a77af474f7c264553a5aeef37ba73b86e8cbf32a
obertChen998 <robertchen998@gmail.com>
2024-01-31 11:24:26 +0800

    Update

---
+++ Circuits/Sequential Logic/Latches and Flip-Flops/Create circuit from truth table.v
@@ -0,0 +1,20 @@
+module top_module (
+    input clk,
+    input j,
+    input k,
+    output reg Q); 
+
+always @(posedge clk) begin
+
+case ({j,k})
+
+    2'b00: Q <= Q;
+    2'b01: Q <= 0;
+    2'b10: Q <= 1;
+    default: Q <= ~Q;
+
+endcase
+
+end
+
+endmodule
