380db4b9ef0cd64c46b97b5836f9191d10d7ff31
obertChen998 <robertchen998@gmail.com>
2024-02-06 22:27:49 +0800

    Update

--- Verilog Language/Module Hierarchy/Module shift.v
+++
@@ -1,7 +0,0 @@
-module top_module ( input clk, input d, output q );
-wire q_1_2,q_2_3;
-    my_dff d1 (.clk(clk),.d(d),.q(q_1_2));
-    my_dff d2 (.clk(clk),.d(q_1_2),.q(q_2_3));
-    my_dff d3 (.clk(clk),.d(q_2_3),.q(q));
-    
-endmodule
