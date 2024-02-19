bba50f3124e56e5b586e700daa9e90e7295c121b
obertChen998 <robertchen998@gmail.com>
2024-02-19 16:48:04 +0800

    Update

--- Circuits/Sequential Logic/Latches and Flip-Flops/Edge capture register.v
+++ Circuits/Sequential Logic/Latches and Flip-Flops/Edge capture register.v
@@ -1,16 +1,17 @@
 module top_module (
     input clk,
-    input d,
-    output reg q
+    input reset,
+    input [31:0] in,
+    output reg [31:0] out
 );
+    reg [31:0] in_last;
+    always @(posedge clk) begin
+        in_last <= in;
+        if(reset) out<=0;
+        else begin
+            out <= out | in_last & ~in;
+        end
 
-always @(posedge clk or negedge clk) begin
-
-q <= d;
-
-end
-
-
-
-
+    end
+    
 endmodule
